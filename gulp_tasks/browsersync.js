170a9969ea1b74f45b882b0a821647c1eaaaa2e3
bvasilop <bvasilop@gmail.com>
2017-08-29 13:36:16 -0700

    added gulp deploy to browerssync.js

--- gulp_tasks/browsersync.js
+++ gulp_tasks/browsersync.js
@@ -2,10 +2,18 @@ const gulp = require('gulp');
 const browserSync = require('browser-sync');
 const spa = require('browser-sync-spa');
 
+const ghPages = require('gulp-gh-pages');
+
+
 const browserSyncConf = require('../conf/browsersync.conf');
 const browserSyncDistConf = require('../conf/browsersync-dist.conf');
 
 browserSync.use(spa());
+
+gulp.task('deploy', function() {
+  return gulp.src('./dist/**/*')
+    .pipe(ghPages());
+});
 
 gulp.task('browsersync', browserSyncServe);
 gulp.task('browsersync:dist', browserSyncDist);
