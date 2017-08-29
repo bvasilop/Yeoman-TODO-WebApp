ff5b9fed782eba1960975ab32aab5b0607474f0b
bvasilop <bvasilop@gmail.com>
2017-08-29 13:25:45 -0700

    changed location of gulp task in gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -10,13 +10,15 @@ const conf = require('./conf/gulp.conf');
 // Load some files into the registry
 const hub = new HubRegistry([conf.path.tasks('*.js')]);
 
-// Tell gulp to use the tasks just loaded
-gulp.registry(hub);
 // deploy project on gh-pages
 gulp.task('deploy', function() {
   return gulp.src('./dist/**/*')
     .pipe(ghPages());
 });
+
+// Tell gulp to use the tasks just loaded
+gulp.registry(hub);
+
 gulp.task('build', gulp.series(gulp.parallel('other', 'webpack:dist')));
 gulp.task('test', gulp.series('karma:single-run'));
 gulp.task('test:auto', gulp.series('karma:auto-run'));
