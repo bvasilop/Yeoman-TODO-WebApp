8c1b8b80ede567fb704f57c164a2fdc6674fa970
bvasilop <bvasilop@gmail.com>
2018-05-11 17:22:29 -0700

    restored working version

--- gulpfile.js
+++ gulpfile.js
@@ -1,41 +1,22 @@
 const gulp = require('gulp');
-const deploy = require('gulp-gh-pages');
 const HubRegistry = require('gulp-hub');
 const browserSync = require('browser-sync');
 
-<<<<<<< HEAD
 const ghPages = require('gulp-gh-pages');
 
-=======
->>>>>>> parent of 59a4f26...  added gulp deploy
 const conf = require('./conf/gulp.conf');
 
 // Load some files into the registry
 const hub = new HubRegistry([conf.path.tasks('*.js')]);
-<<<<<<< HEAD
-<<<<<<< HEAD
 
 // Tell gulp to use the tasks just loaded
 gulp.registry(hub);
-
-
-gulp.task('build', gulp.series(gulp.parallel('other', 'webpack:dist')));
-
 // deploy project on gh-pages
 gulp.task('deploy', () => {
-=======
-// deploy on gh-pages
-gulp.task('deploy', function() {
->>>>>>> parent of 797e66f... deployed on gh-pages
   return gulp.src('./dist/**/*')
     .pipe(ghPages());
 });
-=======
-
-// Tell gulp to use the tasks just loaded
-gulp.registry(hub);
->>>>>>> parent of 59a4f26...  added gulp deploy
-
+gulp.task('build', gulp.series(gulp.parallel('other', 'webpack:dist')));
 gulp.task('test', gulp.series('karma:single-run'));
 gulp.task('test:auto', gulp.series('karma:auto-run'));
 gulp.task('serve', gulp.series('webpack:watch', 'watch', 'browsersync'));
@@ -52,4 +33,3 @@ function watch(done) {
   gulp.watch(conf.path.tmp('index.html'), reloadBrowserSync);
   done();
 }
-
